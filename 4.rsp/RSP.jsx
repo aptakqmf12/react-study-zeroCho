b0c054f2e34f43fd1593aaa8a98f121e7004c78c
aptakqmf12 <leexodhks12@naver.com>
2021-11-24 14:59:33 +0900

    가위바위보 환경설정 + 화면뿌리기 (반복작업남음)

--- 4.rsp/RSP.jsx
+++ 4.rsp/RSP.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import styled from 'styled-components';
 
-function RSP() {
+const VALUE = ['가위', '바위', '보'];
+const SCORE = [-1, 0, 1];
+const ComputerValue = styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    width:300px;
+    height:300px;
+    margin-bottom:10px;
+    border:1px black solid;
+    background-color : ${props => props.bgc};
+    font-size:20px;
+`;
+
+const RSP = () => {
+    const [computer, setComputer] = useState({ value : VALUE[0], score : SCORE[0]});
+    const intervalTime = useRef();
+
+    useEffect( () => {
+        //componentDidMount, componentDidUpdate
+        intervalTime.current = setInterval( () => {
+            setComputer( { value : VALUE[1], score: SCORE[1]} )
+        },1000)
+        return () => {
+            //componentWillUnmount
+        } 
+    }, [computer])
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <ComputerValue bgc={'red'}>{computer.value} {computer.score}</ComputerValue>
+            <button>가위</button>
+            <button>바위</button>
+            <button>보</button>
+        </>
     )
 }
 
-export default RSP
+export default RSP;
